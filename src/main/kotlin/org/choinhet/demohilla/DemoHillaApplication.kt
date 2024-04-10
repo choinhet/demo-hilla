d23159aa74d2bb7379d3af973fefa9e78df7c074
Rafael Choinhet <rafael.choinhet@looqbox.com>
2024-04-10 09:05:14 -0300

    chore: add base style

--- src/main/kotlin/org/choinhet/demohilla/DemoHillaApplication.kt
+++ src/main/kotlin/org/choinhet/demohilla/DemoHillaApplication.kt
@@ -1,10 +1,13 @@
 package org.choinhet.demohilla
 
+import com.vaadin.flow.component.page.AppShellConfigurator
+import com.vaadin.flow.theme.Theme
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoHillaApplication
+@Theme(value = "hilla-react-app")
+class DemoHillaApplication: AppShellConfigurator
 
 fun main(args: Array<String>) {
     runApplication<DemoHillaApplication>(*args)
