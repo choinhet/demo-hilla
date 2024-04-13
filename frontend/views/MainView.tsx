907eda2128b72ccd47d3baef41c5800e31ee449f
Rafael Choinhet <rafael.choinhet@looqbox.com>
2024-04-13 19:36:54 -0300

    chore: remove custom impl

--- frontend/views/MainView.tsx
+++ frontend/views/MainView.tsx
@@ -1,29 +1,16 @@
 import {VerticalLayout} from "@hilla/react-components/VerticalLayout";
-import React, {useCallback} from "react";
-import {Button, ButtonElement} from "@hilla/react-components/Button";
-import {Notification} from "@hilla/react-components/Notification";
-import {HelloEndpoint} from "Frontend/generated/endpoints";
+import React from "react";
+import {Button} from "@hilla/react-components/Button";
 
-export default function MainView() {
-    const onButtonClick = useCallback((event: React.MouseEvent<ButtonElement, MouseEvent>) => {
-        const buttonName = event.currentTarget.textContent || 'Undefined Button Clicked';
-        Notification.show(`${buttonName} clicked!`, {
-            position: 'middle',
-            duration: 1000,
-            theme: 'success',
-        })
-        HelloEndpoint.buttonClicked(buttonName).then()
-    }, []);
 
-    return (
-        <VerticalLayout
-            theme="spacing padding"
-            className="height-4xl"
-            style={{ justifyContent: 'center' }}
-        >
-            <Button onClick={onButtonClick}>Button 1</Button>
-            <Button onClick={onButtonClick}>Button 2</Button>
-            <Button onClick={onButtonClick}>Button 3</Button>
+const MainView = () => {
+     return (
+        <VerticalLayout theme="spacing padding">
+            <Button>Button 1</Button>
+            <Button>Button 2</Button>
+            <Button>Button 3</Button>
         </VerticalLayout>
     );
 }
+
+export default MainView;
